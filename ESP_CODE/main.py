305bc65412af7058670a15d08695baaac240b29b
wolfino65 <wolfino6565@gmail.com>
2025-02-03 18:34:04 +0100

    API test
    Simple api test. Will be used for device conf.

--- ESP_CODE/main.py
+++ ESP_CODE/main.py
@@ -1,1 +1,14 @@
+import network
+from microdot import Microdot
 print("hi from main")
+app= Microdot()
+
+@app.route("/")
+def start(request):
+    return "access"
+
+w=network.WLAN(network.WLAN.IF_AP)
+w.config(ssid="test")
+w.active(True )
+print(w.ifconfig())
+app.run(port=60)
