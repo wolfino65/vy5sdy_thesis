b7376af6f56e32a4123042404582dc81b7f725a8
wolfino65 <wolfino6565@gmail.com>
2025-02-23 11:11:38 +0100

    module connection

--- ESP_CODE/main.py
+++ ESP_CODE/main.py
@@ -1,16 +1,19 @@
 print("switch to main")
+import machine
 #----------------------
 populated=[False,False,False,False] #module connections
+con_check_pins=[1,2,3,4]
+module_controlfile_names = ["a.py","b.py","c.py","d.py"]
 #----------------------
 import urequests
 FILE_ID = "1F5GJ0k45eJiRC3-XUQ_f74drGBJRwOTh"
 API_KEY = "AIzaSyCof7aLYawxMevJrPAwpgYt9fUxYXMrdZE"
-def download_file_from_google_drive(file_id, api_key):
+def download_file_from_google_drive(file_id, api_key,file):
     url = f"https://www.googleapis.com/drive/v3/files/{file_id}?alt=media&key={api_key}"
     response = urequests.get(url)
     
     if response.status_code == 200:
-        with open("asd"+".py", "wb") as f:
+        with open(file, "wb") as f:
             f.write(response.content)
         print("File downloaded successfully")
     else:
@@ -18,7 +21,12 @@ def download_file_from_google_drive(file_id, api_key):
         print("Status Code:", response.status_code)
         print("Response:", response.text)
     response.close()
-download_file_from_google_drive(FILE_ID,API_KEY)
+#download_file_from_google_drive(FILE_ID,API_KEY)
 
-import asd as a
-a.set_to_red()
+def connect_new_module(fid)
+    while True:
+        for i in range(0,len(con_check_pins)-1):
+            p= machine.Pin(con_check_pins[i],machine.Pin.IN)
+            if p.value() == 1 and populated[i] == False :
+                populated[i]=True
+                download_file_from_google_drive(fid,API_KEY,module_controlfile_names[i])
