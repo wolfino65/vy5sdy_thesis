107d90a8fb7053749bd7582b927cde53a953842d
wolfino65 <wolfino6565@gmail.com>
2025-03-14 13:29:52 +0100

    implemented long polling

--- MIDDLEWARE__APIs/API/Routing/task_endpoints.js
+++ MIDDLEWARE__APIs/API/Routing/task_endpoints.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import { addTask, getTasksByDeviceId, deleteTask } from '../DB_handling/tasks_db.js'
 var taskRouter = express.Router();
-
+var responses = []
 taskRouter.post('/addTask', async (req, res) => {
     let { dev_id, module_id, user_id, params, aditionalInfo } = req.body;
     let result = await addTask(dev_id, module_id, user_id, params, aditionalInfo);
+    checkForData(dev_id)
     res.send(result);
 })
 
 taskRouter.get('/getTasksByDeviceId', async (req, res) => {
     let { dev_id } = req.headers;
     let result = await getTasksByDeviceId(dev_id);
-    res.send(result);
+
+    if (JSON.stringify(result)==='[]') {
+        responses.push({
+            "dev_id": dev_id,
+            "res": res
+        })
+    }
+    else {
+        res.send(result);
+    }
 })
 
 taskRouter.delete('/deleteTask', async (req, res) => {
@@ -19,5 +29,15 @@ taskRouter.delete('/deleteTask', async (req, res) => {
     let result = await deleteTask(task_id);
     res.send(result);
 })
+
+async function checkForData(requesting_dev_id) {
+    for (const element of responses) {
+        if (element['dev_id'] === requesting_dev_id) {
+            var resp = element['res']
+            let result = await getTasksByDeviceId(element['dev_id']);
+            resp.send(result)
+        }
+    }
+}
 
 export default taskRouter;
