86065d25cfe78729661a25b116f799f06556ac83
Farkas Levente <79054387+wolfino65@users.noreply.github.com>
2025-04-12 22:56:15 +0200

    Update dbCon.js

--- MIDDLEWARE__APIs/API/DB/dbCon.js
+++ MIDDLEWARE__APIs/API/DB/dbCon.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb"
 
-const client = new MongoClient("mongodb+srv://wolfino6565:Dell25L65!@szakdolgozat.uqwvi.mongodb.net/?retryWrites=true&w=majority&appName=szakdolgozat");
+const client = new MongoClient("mongodb+srv://wolfino6565:<password>@szakdolgozat.uqwvi.mongodb.net/?retryWrites=true&w=majority&appName=szakdolgozat");
 try {
     await client.connect();
 }
